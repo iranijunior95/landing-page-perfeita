8249e1902d6ae66f0b2008df7f9e196fef95b38a
Irani Junior <iranijunior95@gmail.com>
2025-06-22 11:06:40 -0300

    construção menu mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonMenu from "../ButtonMenu";
 import "./style.css";
 
 function Header() {
     const [linkDropdown, setLinkDropdown] = useState(false);
+    const [mobileMenu, setMobileMenu] = useState(false);
 
-    function cliqueDropdown(event) {
+    function clickDropdown(event) {
         event.preventDefault();
-
         setLinkDropdown(!linkDropdown);
     }
+
+    function clickMobileMenu() {
+        setMobileMenu(!mobileMenu);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            if(window.innerWidth > 850) {
+                setMobileMenu(false);
+            }
+
+            if(window.innerWidth < 850) {
+                setLinkDropdown(false);
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        mobileMenu ? document.body.classList.add('menu-open') : document.body.classList.remove('menu-open');
+    }, [mobileMenu]);
 
     return (
         <header>
@@ -35,7 +55,7 @@ function Header() {
                             <a 
                                 href="#"
                                 className="link-dropdown"
-                                onClick={cliqueDropdown}
+                                onClick={clickDropdown}
                             >
                                 Serviços
                                 <img src="/public/img/Chevron_Down.png" alt="arrow down" />
@@ -70,7 +90,71 @@ function Header() {
                         <ButtonMenu typeButton="registrar">Registrar</ButtonMenu>
                     </div>
                 </div>
+
+                <button 
+                    type="button"
+                    className="btn-mobile"
+                    onClick={clickMobileMenu}
+                >
+                   <img src="/public/img/icon_menu.png" alt="" /> 
+                </button>
             </nav>
+
+            <div 
+                className={mobileMenu ? "menu-mobile active" : "menu-mobile"}
+            >
+                <div 
+                    className={mobileMenu ? "menu-mobile-container active" : "menu-mobile-container"}
+                >
+                    <ul>
+                        <li>
+                            <a href="#">Início</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Sobre</a>
+                        </li>
+
+                        <li className="li-dropdown">
+                            <a 
+                                href="#"
+                                className="link-dropdown"
+                                onClick={clickDropdown}
+                            >
+                                Serviços
+                                <img src="/public/img/Chevron_Down.png" alt="arrow down" />
+                            </a>
+
+                            <div 
+                                className={linkDropdown ? "div-dropdown active" : "div-dropdown"}
+                            >
+                                <ul>
+                                    <li>
+                                        <a href="#">Link 01</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#">Link 02</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#">Link 03</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li>
+                            <a href="#">Contatos</a>
+                        </li>
+                    </ul>
+
+                    <div className="button-menu-mobile">
+                        <ButtonMenu typeButton="login">Login</ButtonMenu>
+                        <ButtonMenu typeButton="registrar">Registrar</ButtonMenu>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
